0a9f7ac324535823cc3d4601dea7e7b0e88bde03
Lilian POULIQUEN <lil.pouliqu@gmail.com>
2021-03-04 21:44:09 +0100

    feat(commands): implémentation API pour assigner

--- app/commands/assigner.js
+++ app/commands/assigner.js
@@ -1,4 +1,5 @@
 const cmdConfig = require("./cmd_config.json");
+const http = require("http");
 module.exports = {
     name: "assigner",
     description: "Assigne le rôle spécifié à la personne spécifiée",
@@ -10,19 +11,29 @@ module.exports = {
         let excludedRoleIds = [
             cmdConfig.idRoleAdmin,
             cmdConfig.idRoleGameMaster,
-            message.channel.guild.roles.cache.find(role => role.name === "@everyone").id
+            cmdConfig.idRoleEveryone
         ];
 
         lstPlayersToAssign.forEach(player => {
-            if (roleToAssign.id === cmdConfig.idRoleDead && !player.roles.cache.has(cmdConfig.idRoleReaper)) {
-                player.roles.cache.forEach(role => {
-                    if (excludedRoleIds.indexOf(role.id) === -1) {
-                        player.roles.remove(role);
-                    }
-                });
-            }
-            player.roles.add(roleToAssign)
-        })
+            getRolesByPlayer(player.id).then(lstRoles => {
+                if ((roleToAssign.id === cmdConfig.idRoleDead) && (-1 === lstRoles.indexOf(cmdConfig.idRoleReaper))) {
+                    lstRoles.forEach(role => {
+                        if (-1 === excludedRoleIds.indexOf(role.idrole)) {
+                            player.roles.remove(role.idrole)
+                                .then(() => {
+                                    deleteAssignement({ idPlayer: player.id, idRole: role.idrole });
+                                })
+                                .catch(error => { throw error });
+                        }
+                    });
+                }
+                player.roles.add(roleToAssign)
+                    .then(() => {
+                        assignRole({ idPlayer: player.id, idRole: roleToAssign.id });
+                    })
+                    .catch(error => { throw error });
+            });
+        });
     }
 }
 
@@ -34,4 +45,87 @@ function getMapRoles(guildRoles) {
     roles.set("env", guildRoles.resolve(cmdConfig.idRoleEnchanted));
     roles.set("mort", guildRoles.resolve(cmdConfig.idRoleDead));
     return roles;
+}
+
+function getRolesByPlayer(idPlayer) {
+    return new Promise((resolve, reject) => {
+        let url = `http://php-api/services.php?service=getRolesByPlayer&idPlayer=${idPlayer}`;
+        http.get(url, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: ", err.message);
+        });
+    });
+}
+
+function deleteAssignement(assignement) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            protocol: 'http:',
+            hostname: 'php-api',
+            port: 80,
+            path: `/services.php?service=deleteAssignement&idPlayer=${assignement.idPlayer}&idRole=${assignement.idRole}`,
+            method: 'DELETE'
+        };
+
+        const req = http.request(options, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: ", err.message);
+        });
+
+        req.end();
+    })
+}
+
+function assignRole(assignement) {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(assignement);
+        const options = {
+            protocol: 'http:',
+            hostname: 'php-api',
+            port: 80,
+            path: `/services.php?service=assignRole`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const req = http.request(options, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                resolve(JSON.parse(data));
+            });
+
+        }).on("error", (err) => {
+            console.log("Error: ", err.message);
+        });
+
+        req.write(data);
+        req.end();
+    });
 }
