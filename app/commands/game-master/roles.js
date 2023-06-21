b1b5a52c29884beddc03070b77d4044173ffb1a8
Lilian POULIQUEN <lil.pouliqu@gmail.com>
2023-06-22 01:30:12 +0200

    refactor(commands): Updated roles and joueurs to discord.js 14(the two of them are in command roles now)

---
+++ app/commands/game-master/roles.js
@@ -0,0 +1,58 @@
+const cmdConfig = require("./cmd_config.json");
+const { SlashCommandBuilder, Integration } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roles')
+        .setDescription('Affiche les rôles et leurs membres dans le cannal "maitre-du-jeu"'),
+    async execute(interaction) {
+        // App is thinking
+        await interaction.deferReply();
+
+        // Retrieve players
+        const vocalChannel = await interaction.guild.channels.fetch(cmdConfig.idVocalChannelMain);
+        const playersInVocalChannel = vocalChannel.members.filter(user => !user.roles.resolve(cmdConfig.idRoleGameMaster));
+
+        // Other variables
+        const excludedRoleIds = cmdConfig.excludedRoleIds;
+        const channelGameMaster = await interaction.guild.channels.fetch(cmdConfig.idTextChannelGameMaster);
+        const roleCollection = await interaction.guild.roles.fetch();
+
+        // Check if the user is the Game Master
+        // If true, sends the entire list to the Game Master channel
+        // If false, reply to the player with a truncated list
+        if (roleCollection.get(cmdConfig.idRoleGameMaster).members.has(interaction.user.id)) {
+            var channelToSend = channelGameMaster;
+        } else {
+            excludedRoleIds.push(cmdConfig.idRoleLovers);
+            excludedRoleIds.push(cmdConfig.idRoleInfected);
+            excludedRoleIds.push(cmdConfig.idRoleOiled);
+            excludedRoleIds.push(cmdConfig.idRoleEnchanted);
+            excludedRoleIds.push(cmdConfig.idRoleDead);
+            var channelToSend = await interaction.guild.channels.fetch(interaction.channelId);
+        }
+
+        // Filter the role list to keep only the roles that have to be shown
+        const roleCollectionFiltered = roleCollection.filter(role => -1 === excludedRoleIds.indexOf(role.id));
+
+        // Check if there is at least one player
+        if (0 === playersInVocalChannel.size) {
+            await interaction.editReply('Il n\'y a aucun joueur actuellement');
+        } else {
+            // For each game role, display who are its members
+            for await (const [roleId, role] of roleCollectionFiltered) {
+                if (0 !== role.members.size) {
+                    let message = `Rôle ${role.name} :\`\`\`\n`;
+                    for await (const [memberId, member] of role.members) {
+                        if (playersInVocalChannel.has(memberId)) {
+                            message += `– ${member.nickname}\n`;
+                        }
+                    }
+                    message += '```';
+                    channelToSend.send(message);
+                }
+            }
+            await interaction.editReply('Affichage terminé !');
+        }
+    }
+}
