339978d7516c1bb231dec56c3327d2d88e43e73b
Lilian POULIQUEN <lil.pouliqu@gmail.com>
2023-06-26 01:28:56 +0200

    docs(deploy-commands): Improved logging

--- app/deploy-commands.js
+++ app/deploy-commands.js
@@ -1,12 +1,13 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+const { REST, Routes } = require('discord.js');
+const { getLogDate } = require('./functions');
+const { clientId, guildId, token } = require('./config.json');
 
 // Load commands
 const commands = [];
 const commandFoldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(commandFoldersPath);
+const commandFolders = fs.readdirSync(commandFoldersPath).filter(folder => fs.statSync(path.join(commandFoldersPath, folder)).isDirectory());
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(commandFoldersPath, folder);
@@ -17,7 +18,7 @@ for (const folder of commandFolders) {
 		if ('data' in command && 'execute' in command) {
 			commands.push(command.data.toJSON());
 		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			console.log(`${getLogDate()} WARNING: The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
 }
@@ -26,7 +27,7 @@ for (const folder of commandFolders) {
 const rest = new REST().setToken(token);
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`${getLogDate()} INFO: Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
@@ -34,7 +35,7 @@ const rest = new REST().setToken(token);
 			{ body: commands },
 		);
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`${getLogDate()} INFO: Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
 		console.error(error);
 	}
