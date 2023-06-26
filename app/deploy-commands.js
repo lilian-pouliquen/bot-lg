dbedd8feb2aa64bc6f19c71fa0abec25660e375b
Lilian POULIQUEN <lil.pouliqu@gmail.com>
2023-06-26 21:46:07 +0200

    docs(logs): Reworked the log system

--- app/deploy-commands.js
+++ app/deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { REST, Routes } = require('discord.js');
-const { getLogDate } = require('./functions');
+const { createLog } = require('./functions');
 const { clientId, guildId, token } = require('./config.json');
 
 // Load commands
@@ -18,7 +18,7 @@ for (const folder of commandFolders) {
 		if ('data' in command && 'execute' in command) {
 			commands.push(command.data.toJSON());
 		} else {
-			console.log(`${getLogDate()} WARNING: The command at ${filePath} is missing a required "data" or "execute" property.`);
+			createLog('global', 'bot-lg', 'warn', `The command at ${filePath} is missing a required "data" or "execute" property`);
 		}
 	}
 }
@@ -27,16 +27,16 @@ for (const folder of commandFolders) {
 const rest = new REST().setToken(token);
 (async () => {
 	try {
-		console.log(`${getLogDate()} INFO: Started refreshing ${commands.length} application (/) commands.`);
-
+		createLog('global', 'bot-lg', 'info', `Started refreshing ${commands.length} application commands`);
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
 
-		console.log(`${getLogDate()} INFO: Successfully reloaded ${data.length} application (/) commands.`);
+		createLog('global', 'bot-lg', 'info', `Successfully reloaded ${data.length} application commands`);
 	} catch (error) {
 		console.error(error);
+		createLog('global', 'bot-lg', 'error', error);
 	}
 })();
